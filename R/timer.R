e445bc06f5a2f0e5869cdcfee4d767d57530b663
cimentadaj <cimentadaj@gmail.com>
2017-01-24 11:06:02 +0100

    Added timer.R function

---
+++ R/timer.R
@@ -0,0 +1,22 @@
+#' A stopwatch
+#'
+#' @param mins a number. can be minutes or seconds.
+#' @param sound a number specifying the ring sound. See ?beepr::beep
+#'
+#' @return A ringtone will go off when the time is off.
+#' @export
+#'
+#' @examples timer(1, 8) # 1 minute
+#' timer(0.3) # 30 seconds
+timer <- function(mins, sound = 8) {
+
+total_mins <- ifelse(abs(mins - round(mins)) < .Machine$double.eps^0.5, mins * 60, mins * 100)
+
+timeisup <- Sys.time() + total_mins
+
+while(Sys.time() < timeisup) {
+# do nothing
+}
+
+beepr::beep(sound)
+}
