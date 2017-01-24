8bfbd92bc976fc7e617b7c3980ea18687058fd5c
cimentadaj <cimentadaj@gmail.com>
2017-01-24 12:48:49 +0100

    Fixed seconds specification + added 2 examples

--- R/timer.R
+++ R/timer.R
@@ -6,8 +6,10 @@
 #' @return A ringtone will go off when the time is off.
 #' @export
 #'
-#' @examples timer(1, 8) # 1 minute
+#' @examples
+#' timer(1, sound = 8) # 1 minute
 #' timer(0.3) # 30 seconds
+#' timer(0.05) # 5 seconds
 timer <- function(mins, sound = 8) {
 
 total_mins <- ifelse(abs(mins - round(mins)) < .Machine$double.eps^0.5, mins * 60, mins * 100)
