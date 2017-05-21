1b99b3934a3a80e15b7092aec37435d84b466845
cimentadaj <cimentadaj@gmail.com>
2017-05-21 18:41:25 +0200

    Removed timer example which takes a lot of example

--- R/timer.R
+++ R/timer.R
@@ -7,9 +7,11 @@
 #' @export
 #'
 #' @examples
-#' timer(1, sound = 8) # 1 minute
-#' timer(0.3) # 30 seconds
-#' timer(0.05) # 5 seconds
+#' # Not run
+# timer(1, sound = 8) # 1 minute
+# timer(0.3) # 30 seconds
+# timer(0.05) # 5 seconds
+#' # Not run
 timer <- function(mins, sound = 8) {
 
 total_mins <- ifelse(abs(mins - round(mins)) < .Machine$double.eps^0.5, mins * 60, mins * 100)
