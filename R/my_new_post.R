2e27623fa3fcbc56c8a4b1b5a0c69b5f942061e3
cimentadaj <cimentadaj@gmail.com>
2017-11-17 12:24:40 +0100

    Add my_new_post fun for blogdown posting

---
+++ R/my_new_post.R
@@ -0,0 +1,22 @@
+#' Insert a new post using blogdown
+#'
+#' @param title Your blogpost's title
+#' @param ... All extra arguments passed to blogdown::new_post
+#'
+#' @return It creates a .Rmd and .html file under ./blog/your-title-name/
+#' @export
+#'
+#' @examples
+my_new_post <- function(title, ...) {
+
+  title_change <- trimws(gsub("[[:punct:]]", "", title))
+  slug <- stringr::str_to_lower(gsub("\\s","-", title_change))
+
+  dir <- paste0(lubridate::today(), "-", slug)
+
+  blogdown::new_post(title = title,
+                     author = "Jorge Cimentada",
+                     subdir = file.path("./blog", dir),
+                     ext = ".Rmd",
+                     ...)
+}
