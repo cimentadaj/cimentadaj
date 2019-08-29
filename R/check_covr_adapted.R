a6791f868429978013ac01b0710f2834df1476ce
Jorge Cimentada <cimentadaj@gmail.com>
2019-08-29 08:31:23 +0200

    Adds adapted test and test_file

--- R/check_covr_adapted.R
+++ R/check_covr_adapted.R
@@ -11,6 +11,32 @@ adapted_check <- function(..., run_tests = 'true') {
   devtools::check(...)
 }
 
+##' @title Use devtools::test running testthat::skip_on_cran() tests
+##' @param ... arguments passed to devtools::test
+##' @param run_tests value of the NOT_CRAN environment variable. This should be
+##' either 'true' or 'false'
+##' @return results from devtools::test
+##' @author Jorge Cimentada
+##' @export
+adapted_test <- function(..., run_tests = 'true') {
+  on.exit(Sys.setenv("NOT_CRAN" = Sys.getenv("NOT_CRAN")))
+  Sys.setenv("NOT_CRAN" = run_tests)
+  devtools::test(...)
+}
+
+##' @title Use devtools::test_file running testthat::skip_on_cran() tests
+##' @param ... arguments passed to devtools::test_file
+##' @param run_tests value of the NOT_CRAN environment variable. This should be
+##' either 'true' or 'false'
+##' @return results from devtools::test_file
+##' @author Jorge Cimentada
+##' @export
+adapted_test_file <- function(..., run_tests = 'true') {
+  on.exit(Sys.setenv("NOT_CRAN" = Sys.getenv("NOT_CRAN")))
+  Sys.setenv("NOT_CRAN" = run_tests)
+  devtools::test_file(...)
+}
+
 ##' @title Use covr::codecov while running testthat::skip_on_cran() tests
 ##' @param ... arguments passed to covr::codecov
 ##' @param run_tests value of the NOT_CRAN environment variable. This should be
